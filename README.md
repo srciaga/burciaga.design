1657e00fa272dc1b0549a99a12a5f66f40360d5f
srciaga <srciaga@gmail.com>
2023-02-17 01:18:10 -0800

    Updated base template and content.

--- README.md
+++
@@ -1,3 +0,0 @@
-My design portfolio.
-
-Work in progress.
---
+++ js/nav.js
@@ -0,0 +1,13 @@
+// Wait for the page to finish loading
+window.addEventListener('load', function() {
+    // Get the toggle button and navbar links
+    const toggleButton = document.querySelector('.toggle');
+    const navbarLinks = document.querySelector('.links');
+  
+    // Add a click event listener to the toggle button
+    toggleButton.addEventListener('click', function() {
+      // Toggle the 'active' class on the navbar links
+      navbarLinks.classList.toggle('active');
+    });
+  });
+  
